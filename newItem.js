a111677a24d8529f1214b879f2292d26e8a6d17a
AngelTheCreator <angel_guevara94@hotmail.com>
2022-11-22 14:58:47 -0800

    fixed the position of the items

--- newItem.js
+++ newItem.js
@@ -1,10 +1,10 @@
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-    item.addEventListener('click', function(){
+function newItem(url){
+    let item = newImage(url)
+    item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
-        inventoryItem.src = url
+        inventoryItem.src = url;
         inventory.append(inventoryItem)
-
     })
+    return item
 }
